31eeb3f193616feb9c8c7187f15f8cf99c91cea5
Kniffen <kniffen@knifftech.org>
2024-12-06 17:50:48 +0100

    Day 006: Part 2

--- Day006.java
+++ Day006.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 class Tile {
   int index;
@@ -17,10 +20,10 @@ class Position {
   int y;
   int direction;
 
-  Position(int x, int y) {
+  Position(int x, int y, int direction) {
     this.x = x;
     this.y = y;
-    this.direction = 0;
+    this.direction = direction;
   }
 }
 
@@ -39,17 +42,19 @@ public class Day006 {
   };
   static String[] input = Utils.readFile("day006.txt");
 
-  static int part1(String[] data) {
+  static Tile[] generateTiles(String[] data, int rows, int columns) {
     Tile[] tiles = new Tile[data.length * data[0].length()];
-    int columns = data[0].length();
-    int rows = data.length;
     for (int y = 0; y < rows; y++) {
       for (int x = 0; x < columns; x++) {
         tiles[x + y * columns] = new Tile(x, y, columns, data[y].charAt(x));
       }
     }
 
-    Position guardPosition = new Position(0,0);
+    return tiles;
+  }
+
+  static Position getGuardStartingPosition(String[] data, Tile[] tiles, int rows, int columns) {
+    Position guardPosition = new Position(0,0,0);
     for (int y = 0; y < rows; y++) {
       for (int x = 0; x < columns; x++) {
         if (data[y].charAt(x) == '^') {
@@ -61,6 +66,15 @@ public class Day006 {
         }
       }
     }
+
+    return guardPosition;
+  }
+
+  static Stream<Tile> part1(String[] data) {
+    int columns = data[0].length();
+    int rows = data.length;
+    Tile[] tiles = generateTiles(data, rows, columns);
+    Position guardPosition = getGuardStartingPosition(data, tiles, rows, columns);
 
     while (guardPosition.x >= 0 && guardPosition.x < columns && guardPosition.y >= 0 && guardPosition.y < rows) {
       int index = guardPosition.x + guardPosition.y * columns;
@@ -100,13 +114,82 @@ public class Day006 {
       } while (tiles[nextIndex].isSolid);
     }
 
-    int tilesVisited = (int) Arrays.stream(tiles).filter(tile -> tile.visited).count();
-    return tilesVisited;
+    return Arrays.stream(tiles).filter(tile -> tile.visited);
+  }
+
+  static int part2(String[] data) {
+    int rows = data.length;
+    int columns = data[0].length();
+    Tile[] tiles = generateTiles(data, rows, columns);
+    Position guardStartingPosition = getGuardStartingPosition(data, tiles, rows, columns);
+    Tile[] blockedTiles = part1(data).filter(tile -> tile.index != guardStartingPosition.x + guardStartingPosition.y * columns).toArray(Tile[]::new);
+
+    int loopCount = 0;
+    for (Tile blockedTile : blockedTiles) {
+      ArrayList<Position> positionHistory = new ArrayList<Position>();
+      positionHistory.add(new Position(guardStartingPosition.x, guardStartingPosition.y, guardStartingPosition.direction));
+
+      boolean foundBreakingPoint = false;
+      Position guardPosition = new Position(guardStartingPosition.x, guardStartingPosition.y, guardStartingPosition.direction);
+      while (!foundBreakingPoint) {
+        int tileIndex = guardPosition.x + guardPosition.y * columns;
+        int nextTileIndex = tileIndex;
+        do {
+          int x = guardPosition.x;
+          int y = guardPosition.y;
+
+          switch (guardPosition.direction) {
+            case 0: { y--; break; }
+            case 1: { x++; break; }
+            case 2: { y++; break; }
+            case 3: { x--; break; }
+          }
+
+          if (x < 0 || x >= columns || y < 0 || y >= rows) {
+            guardPosition.x = x;
+            guardPosition.y = y;
+            foundBreakingPoint = true;
+            break;
+          }
+
+          nextTileIndex = x + y * columns;
+          if (tiles[nextTileIndex].isSolid || blockedTile.index == nextTileIndex) {
+            int nextDir = guardPosition.direction + 1;
+            guardPosition.direction = nextDir > 3 ? 0 : nextDir;
+          } else {
+            guardPosition.x = x;
+            guardPosition.y = y;
+          }
+
+          boolean isAlreadyVisited = false;
+          for (Position pos : positionHistory) {
+            if (pos.x == guardPosition.x && pos.y == guardPosition.y && pos.direction == guardPosition.direction) {
+              isAlreadyVisited = true;
+              break;
+            }
+          }
+
+          if (isAlreadyVisited) {
+            foundBreakingPoint = true;
+            loopCount++;
+            System.out.printf("Loop count: %s\n", loopCount);
+            break;
+          }
+
+          positionHistory.add(new Position(guardPosition.x, guardPosition.y, guardPosition.direction));
+        } while (tiles[nextTileIndex].isSolid || blockedTile.index == nextTileIndex);
+      }
+    }
+
+    return loopCount;
   }
 
   static void run() {
     System.out.println("\nDay 006");
-    System.out.printf("Part 1 control: %s\n", part1(testData));
-    System.out.printf("Part 1 answer: %s\n", part1(input));
+    System.out.printf("Part 1 control: %s\n", part1(testData).count());
+    System.out.printf("Part 1 answer: %s\n", part1(input).count());
+    System.out.printf("Part 2 control: %s\n", part2(testData));
+    System.out.printf("Part 2 answer: %s\n", part2(input));
+
   }
 }
--- Main.java
+++ Main.java
@@ -2,17 +2,17 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Advent of Code 2024");
 
-    System.out.println("\nDay 001");
-    System.out.printf("Part 1: %s\n", Day001.part1());
-    System.out.printf("Part 2: %s\n", Day001.part2());
+    // System.out.println("\nDay 001");
+    // System.out.printf("Part 1: %s\n", Day001.part1());
+    // System.out.printf("Part 2: %s\n", Day001.part2());
 
-    System.out.println("\nDay 002");
-    System.out.printf("Part 1: %s\n", Day002.part1());
-    System.out.printf("Part 2: %s\n", Day002.part2());
+    // System.out.println("\nDay 002");
+    // System.out.printf("Part 1: %s\n", Day002.part1());
+    // System.out.printf("Part 2: %s\n", Day002.part2());
 
-    Day003.run();
-    Day004.run();
-    Day005.run();
+    // Day003.run();
+    // Day004.run();
+    // Day005.run();
     Day006.run();
   }
 }
