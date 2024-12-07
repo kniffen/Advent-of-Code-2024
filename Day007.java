7a0be2d573372399c4c9fda0b34df1c021c94fa1
Kniffen <kniffen@knifftech.org>
2024-12-07 12:26:25 +0100

    Day 007: Part 2

--- Day007.java
+++ Day007.java
@@ -1,11 +1,10 @@
-import java.math.BigInteger;
 import java.util.Arrays;
 
 class Parsed007Input {
-  BigInteger sum;
+  long sum;
   int[] values;
 
-  Parsed007Input(BigInteger sum, int[] values) {
+  Parsed007Input(long sum, int[] values) {
       this.sum = sum;
       this.values = values;
   }
@@ -29,7 +28,7 @@ public class Day007 {
     Parsed007Input[] parsedInputs = new Parsed007Input[data.length];
     for (int i = 0; i < data.length; i++) {
       String[] parts = data[i].split(": ");
-      BigInteger sum = new BigInteger(parts[0]);
+      long sum = Long.parseLong(parts[0]);
       int[] values = Arrays.stream(parts[1].split(" ")).mapToInt(Integer::parseInt).toArray();
       Parsed007Input parsedInput = new Parsed007Input(sum, values);
       parsedInputs[i] = parsedInput;
@@ -38,26 +37,54 @@ public class Day007 {
     return parsedInputs;
   }
 
-  static BigInteger calculateExpression(int[] values, int operation) {
-    BigInteger result = BigInteger.valueOf(values[0]);
+  static int getMaximumCombinations(int length, int operators) {
+    int maxCombinations = (int) Math.pow(operators, length - 1);
+    return maxCombinations;
+  }
+
+  static long calculateExpression(int[] values, int operation) {
+    long result = values[0];
     for (int i = 0; i < values.length-1; i++) {
-      BigInteger value = BigInteger.valueOf(values[i+1]);
+      long value = values[i+1];
       if (((operation >> i) & 1) == 1) {
-        result = result.add(value);
+        result += value;
       } else {
-        result = result.multiply(value);
+        result *= value;
       }
     }
 
     return result;
   }
 
-  static boolean evaluateCombinations(int[] values, BigInteger target) {
-    int operationCount = values.length - 1;
-    int maxCombinations = 1 << operationCount;
+  static long calculateExpressionPart2(int[] values, int operation) {
+    long result = values[0];
+    int j = operation;
+    for (int i = 0; i < values.length-1; i++) {
+      long value = values[i+1];
+      switch (j % 3) {
+        case 0: {
+          result += value;
+          break;
+        }
+        case 1: {
+          result *= value;
+          break;
+        }
+        case 2: {
+          String combined = result + "" + value;
+          result = Long.parseLong(combined);
+          break;
+        }
+      }
+      j /= 3;
+    }
+    return result;
+  }
+
+  static boolean evaluateCombinations(int[] values, int maxCombinations, long target, boolean combine) {
     for (int i = 0; i < maxCombinations; i++) {
-      BigInteger sum = calculateExpression(values, i);
-      if (sum.equals(target)) {
+      long sum = combine ? calculateExpressionPart2(values, i) : calculateExpression(values, i);
+      if (sum == target) {
         return true;
       }
     }
@@ -65,25 +92,54 @@ public class Day007 {
     return false;
   }
 
-  public static BigInteger part1(String[] data) {
-    BigInteger sum = BigInteger.ZERO;
+  public static long part1(String[] data) {
+    long sum = 0;
 
     Parsed007Input[] validInstructions =
       Arrays
         .stream(parseInput(data))
-        .filter(parsedInput -> evaluateCombinations(parsedInput.values, parsedInput.sum))
+        .filter(parsedInput -> evaluateCombinations(parsedInput.values, getMaximumCombinations(parsedInput.values.length, 2), parsedInput.sum, false))
         .toArray(Parsed007Input[]::new);
 
     for (Parsed007Input validInstruction : validInstructions) {
-      sum = sum.add(validInstruction.sum);
+      sum = sum + validInstruction.sum;
     }
 
      return sum;
+  }
+
+  public static long part2(String[] data) {
+    long sum = 0;
+
+    Parsed007Input[] validInstructions =
+      Arrays
+        .stream(parseInput(data))
+        .filter(parsedInput -> {
+          int maxCombinations = getMaximumCombinations(parsedInput.values.length, 2);
+          boolean isValid = evaluateCombinations(parsedInput.values, maxCombinations, parsedInput.sum, false);
+          if (isValid) {
+            return true;
+          }
+
+          maxCombinations = getMaximumCombinations(parsedInput.values.length, 3);
+          isValid = evaluateCombinations(parsedInput.values, maxCombinations, parsedInput.sum, true);
+          return isValid;
+        })
+        .toArray(Parsed007Input[]::new);
+
+    for (Parsed007Input validInstruction : validInstructions) {
+      sum = sum + validInstruction.sum;
+    }
+
+    return sum;
   }
 
   static void run() {
     System.out.println("\nDay 007");
     System.out.printf("Part 1 control: %s\n", part1(testData));
     System.out.printf("Part 1 answer: %s\n", part1(input));
+    System.out.printf("Part 2 control: %s\n", part2(testData));
+    System.out.printf("Part 2 answer: %s\n", part2(input));
+
   }
 }
