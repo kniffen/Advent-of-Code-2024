5aa261c993743ad3e072ea97d432a4760918e8ef
Kniffen <kniffen@knifftech.org>
2024-12-08 09:00:11 +0100

    Day 008: Part 2

--- Day008.java
+++ Day008.java
@@ -79,12 +79,48 @@ public class Day008 {
     return antiNodes;
   }
 
-  public static int part1(String[] data) {
-    int rows = data.length;
-    int columns = data[0].length();
-    HashMap<Character, ArrayList<Node>> map = parseInput(data, rows, columns);
-    ArrayList<Node> antiNodes = findAntiNodes(map);
+  static ArrayList<Node> findAntiNodesPart2(HashMap<Character, ArrayList<Node>> map, int rows, int columns) {
+    ArrayList<Node> antiNodes = new ArrayList<Node>();
+    for (char key : map.keySet()) {
+      ArrayList<Node> nodes = map.get(key);
+      for (int i = 0; i < nodes.size() - 1; i++) {
+        Node node1 = nodes.get(i);
+        for (int j = i + 1; j < nodes.size(); j++) {
+          Node node2 = nodes.get(j);
 
+          antiNodes.add(node1);
+          antiNodes.add(node2);
+
+
+          int distX = node2.x - node1.x;
+          int distY = node2.y - node1.y;
+          int x1 = node1.x - distX;
+          int y1 = node1.y - distY;
+          int x2 = node2.x + distX;
+          int y2 = node2.y + distY;
+
+          while (
+            (x1 >= 0 && x1 < columns && y1 >= 0 && y1 < rows) ||
+            (x2 >= 0 && x2 < columns && y2 >= 0 && y2 < rows)
+          ) {
+            Node node3 = new Node(x1, y1);
+            Node node4 = new Node(x2, y2);
+            antiNodes.add(node3);
+            antiNodes.add(node4);
+            x1 -= distX;
+            y1 -= distY;
+            x2 += distX;
+            y2 += distY;
+          }
+        }
+      }
+
+    }
+
+    return antiNodes;
+  }
+
+  static void cleanupAnyNodes(ArrayList<Node> antiNodes, int rows, int columns) {
     // Remove anti-nodes that are outside the grid
     antiNodes.removeIf(node -> node.x < 0 || node.x >= columns || node.y < 0 || node.y >= rows);
 
@@ -99,6 +135,24 @@ public class Day008 {
         }
       }
     }
+  }
+
+  public static int part1(String[] data) {
+    int rows = data.length;
+    int columns = data[0].length();
+    HashMap<Character, ArrayList<Node>> map = parseInput(data, rows, columns);
+    ArrayList<Node> antiNodes = findAntiNodes(map);
+    cleanupAnyNodes(antiNodes, rows, columns);
+
+    return antiNodes.size();
+  }
+
+  public static int part2(String[] data) {
+    int rows = data.length;
+    int columns = data[0].length();
+    HashMap<Character, ArrayList<Node>> map = parseInput(data, rows, columns);
+    ArrayList<Node> antiNodes = findAntiNodesPart2(map, rows, columns);
+    cleanupAnyNodes(antiNodes, rows, columns);
 
     return antiNodes.size();
   }
@@ -107,5 +161,7 @@ public class Day008 {
     System.out.println("\nDay 008");
     System.out.printf("Part 1 control: %s\n", part1(testData));
     System.out.printf("Part 1 answer: %s\n", part1(input));
+    System.out.printf("Part 2 control: %s\n", part2(testData));
+    System.out.printf("Part 2 answer: %s\n", part2(input));
   }
 }
